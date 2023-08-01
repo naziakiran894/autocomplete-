cc84688325d7128f9c21075c215c10c08eb84fde
naumansafi <naumansafi97@gmail.com>
2023-08-01 05:44:13 +0000

    initial-commit

---
+++ src/components/Autocomplete.js
@@ -0,0 +1,315 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import useAutocomplete from "@mui/base/useAutocomplete";
+import CheckIcon from "@mui/icons-material/Check";
+import CloseIcon from "@mui/icons-material/Close";
+import { styled } from "@mui/material/styles";
+import { autocompleteClasses } from "@mui/material/Autocomplete";
+
+const Root = styled("div")(
+  ({ theme }) => `
+  color: ${
+    theme.palette.mode === "dark" ? "rgba(255,255,255,0.65)" : "rgba(0,0,0,.85)"
+  };
+  font-size: 14px;
+`
+);
+
+const Label = styled("label")`
+  padding: 0 0 4px;
+  line-height: 1.5;
+  display: block;
+`;
+
+const InputWrapper = styled("div")(
+  ({ theme }) => `
+  width: 100%;
+  border: 1px solid ${theme.palette.mode === "dark" ? "#434343" : "#d9d9d9"};
+  background-color: ${theme.palette.mode === "dark" ? "#141414" : "#fff"};
+  border-radius: 4px;
+  padding: 1px;
+  display: flex;
+  align-items:center;
+  // flex-wrap: wrap;
+
+  &:hover {
+    border-color: ${theme.palette.mode === "dark" ? "#177ddc" : "#40a9ff"};
+  }
+
+  &.focused {
+    border-color: ${theme.palette.mode === "dark" ? "#177ddc" : "#40a9ff"};
+    box-shadow: 0 0 0 2px rgba(24, 144, 255, 0.2);
+  }
+
+  & input {
+    background-color: ${theme.palette.mode === "dark" ? "#141414" : "#fff"};
+    color: ${
+      theme.palette.mode === "dark"
+        ? "rgba(255,255,255,0.65)"
+        : "rgba(0,0,0,.85)"
+    };
+    height: 30px;
+    box-sizing: border-box;
+    padding: 4px 6px;
+    width: 0;
+    min-width: 30px;
+    flex-grow: 1;
+    border: 0;
+    margin: 0;
+    outline: 0;
+  }
+`
+);
+
+function Tag(props) {
+  const { label, onDelete, ...other } = props;
+
+  return (
+    <div {...other}>
+      <span>{label}</span>
+      <CloseIcon onClick={onDelete} />
+    </div>
+  );
+}
+
+Tag.propTypes = {
+  label: PropTypes.string.isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
+
+const StyledTag = styled(Tag)(
+  ({ theme }) => `
+  display: flex;
+  align-items: center;
+  height: 24px;
+  margin: 2px;
+  line-height: 22px;
+  background-color: ${
+    theme.palette.mode === "dark" ? "rgba(255,255,255,0.08)" : "#fafafa"
+  };
+  border: 1px solid ${theme.palette.mode === "dark" ? "#303030" : "#e8e8e8"};
+  border-radius: 6px;
+  box-sizing: content-box;
+  padding: 0 12px 0 16px;
+  outline: 0;
+  overflow: hidden;
+
+  &:focus {
+    border-color: ${theme.palette.mode === "dark" ? "#177ddc" : "#40a9ff"};
+    background-color: ${theme.palette.mode === "dark" ? "#003b57" : "#e6f7ff"};
+  }
+
+  & span {
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  
+   
+   
+  }
+
+  & svg {
+    font-size: 12px;
+    cursor: pointer;
+    padding: 4px;
+  }
+`
+);
+
+const Listbox = styled("ul")(
+  ({ theme }) => `
+  minWidht: 300px;
+  width:100%; 
+  margin: 2px 0 0;
+  padding: 0;
+  position: absolute;
+  list-style: none;
+  background-color: ${theme.palette.mode === "dark" ? "#141414" : "#fff"};
+  overflow: auto;
+  max-height: 250px;
+  border-radius: 4px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
+  z-index: 1;
+
+  & li {
+    padding: 5px 12px;
+    display: flex;
+
+    & span {
+      flex-grow: 1;
+     
+    }
+
+    & svg {
+      color: transparent;
+    }
+  }
+
+  & li[aria-selected='true'] {
+    background-color: ${theme.palette.mode === "dark" ? "#2b2b2b" : "#fafafa"};
+    font-weight: 600;
+
+    & svg {
+      color: #1890ff;
+    }
+  }
+
+  & li.${autocompleteClasses.focused} {
+    background-color: ${theme.palette.mode === "dark" ? "#003b57" : "#e6f7ff"};
+    cursor: pointer;
+
+    & svg {
+      color: currentColor;
+    }
+  }
+`
+);
+
+export default function CustomizedHook() {
+  const handleInputChange = (event) => {
+    const inputValue = event.target.value;
+    const hasMathSign = /[+\-*/=<>]/.test(inputValue);
+
+    if (hasMathSign) {
+      event.target.value = "";
+    }
+  };
+
+  const {
+    getRootProps,
+    getInputLabelProps,
+    getInputProps,
+    getTagProps,
+    getListboxProps,
+    getOptionProps,
+    groupedOptions,
+    value,
+    focused,
+    setAnchorEl,
+  } = useAutocomplete({
+    id: "customized-hook-demo",
+    defaultValue: [top100Films[1]],
+    multiple: true,
+    freeSolo: true,
+    options: top100Films,
+    getOptionLabel: (option) => option.title,
+  });
+
+  console.log(groupedOptions, value, "dfdf");
+
+  return (
+    <Root>
+      <div {...getRootProps()}>
+        <Label
+          style={{
+            textAlign: "center",
+            fontSize: "20px",
+            margin: "auto",
+            fontWeight: 900,
+            marginTop: "20px",
+          }}
+          {...getInputLabelProps()}
+        >
+          Autocomplete Calculator
+        </Label>
+        <InputWrapper
+          style={{ width: "70%", margin: "auto", marginTop: "20px" }}
+          ref={setAnchorEl}
+          className={focused ? "focused" : ""}
+        >
+          {value.map((option, index) => (
+            <div>
+              {typeof option === "object" ? (
+                <StyledTag label={option.title} {...getTagProps({ index })} />
+              ) : (
+                <span>{option}</span>
+              )}
+            </div>
+          ))}
+          <input {...getInputProps({ onChange: handleInputChange })} />
+        </InputWrapper>
+      </div>
+      {groupedOptions.length > 0 ? (
+        <Listbox {...getListboxProps()}>
+          {groupedOptions.map((option, index) => (
+            <li {...getOptionProps({ option, index })}>
+              <span>{option.title}</span>
+              <CheckIcon fontSize="small" />
+            </li>
+          ))}
+        </Listbox>
+      ) : null}
+    </Root>
+  );
+}
+
+// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
+const top100Films = [
+  { title: "payment processing fee", year: 1994 },
+  { title: "Payroll bonus G&A", year: 1972 },
+  { title: "SUM(payroll G&A)", year: 1974 },
+  { title: "Salary increase month", year: 2008 },
+  { title: "Addition", calculation: "2 + 3" },
+  { title: "Multiplication", calculation: "6 * 5" },
+  { title: "Square Root", calculation: "sqrt(16)" },
+  { title: "Division", calculation: "10 / 2" },
+  { title: "Sine", calculation: "sin(45)" },
+  { title: "Cosine", calculation: "cos(0)" },
+
+  { title: "12 Angry Men", year: 1957 },
+  { title: "Schindler's List", year: 1993 },
+  { title: "Pulp Fiction", year: 1994 },
+  { title: "The Great Dictator", year: 1940 },
+  { title: "Cinema Paradiso", year: 1988 },
+  { title: "The Lives of Others", year: 2006 },
+  { title: "Grave of the Fireflies", year: 1988 },
+  { title: "Paths of Glory", year: 1957 },
+  { title: "Django Unchained", year: 2012 },
+  { title: "The Shining", year: 1980 },
+  { title: "WALL·E", year: 2008 },
+  { title: "American Beauty", year: 1999 },
+  { title: "The Dark Knight Rises", year: 2012 },
+  { title: "Princess Mononoke", year: 1997 },
+  { title: "Aliens", year: 1986 },
+  { title: "Oldboy", year: 2003 },
+  { title: "Once Upon a Time in America", year: 1984 },
+  { title: "Witness for the Prosecution", year: 1957 },
+  { title: "Das Boot", year: 1981 },
+  { title: "Citizen Kane", year: 1941 },
+  { title: "North by Northwest", year: 1959 },
+  { title: "Vertigo", year: 1958 },
+  {
+    title: "Star Wars: Episode VI - Return of the Jedi",
+    year: 1983,
+  },
+  { title: "Reservoir Dogs", year: 1992 },
+  { title: "Braveheart", year: 1995 },
+  { title: "M", year: 1931 },
+  { title: "Requiem for a Dream", year: 2000 },
+  { title: "Amélie", year: 2001 },
+  { title: "A Clockwork Orange", year: 1971 },
+  { title: "Like Stars on Earth", year: 2007 },
+  { title: "Taxi Driver", year: 1976 },
+  { title: "Lawrence of Arabia", year: 1962 },
+  { title: "Double Indemnity", year: 1944 },
+  {
+    title: "Eternal Sunshine of the Spotless Mind",
+    year: 2004,
+  },
+  { title: "Amadeus", year: 1984 },
+  { title: "To Kill a Mockingbird", year: 1962 },
+  { title: "Toy Story 3", year: 2010 },
+  { title: "Logan", year: 2017 },
+  { title: "Full Metal Jacket", year: 1987 },
+  { title: "Dangal", year: 2016 },
+  { title: "The Sting", year: 1973 },
+  { title: "2001: A Space Odyssey", year: 1968 },
+  { title: "Singin' in the Rain", year: 1952 },
+  { title: "Toy Story", year: 1995 },
+  { title: "Bicycle Thieves", year: 1948 },
+  { title: "The Kid", year: 1921 },
+  { title: "Inglourious Basterds", year: 2009 },
+  { title: "Snatch", year: 2000 },
+  { title: "3 Idiots", year: 2009 },
+  { title: "Monty Python and the Holy Grail", year: 1975 },
+];
---
+++ src/pages/Box.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Autocomplete from "../../src/components/Autocomplete";
+import AutocompleteCalculator from "../../src/components/AutocompleteCalculator";
+
+const Box = () => {
+  return (
+    <div>
+      {/* <AutocompleteCalculator /> */}
+      <Autocomplete />
+    </div>
+  );
+};
+
+export default Box;
